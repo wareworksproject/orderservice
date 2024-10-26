73a68d862f64766396d4e6a45271ca30179946f0
wareworksproject <amoolekan02@gmail.com>
2024-10-26 14:03:15 +0100

    Update CustomerserviceApplication.java

--- src/main/java/com/wareworks/orderservice/CustomerserviceApplication.java
+++ src/main/java/com/wareworks/orderservice/CustomerserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerserviceApplication {
+public class OrderserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderserviceApplication.class, args);
